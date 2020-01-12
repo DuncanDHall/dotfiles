8da66a74a0e4c9318ddc285a0723478e2c3a82ac
Duncan Hall <duncandhall@gmail.com>
2020-01-11 23:43:30 -0500

    added journal zsh function for automated journal file creation

---
+++ ohmyzshcustom/functions.zsh
@@ -0,0 +1,27 @@
+# first one is about
+# `> journal my journal entry`
+#
+# be equivalent to this:
+#
+# `> cd path/to/journal && v YYMMDD_my_journal_entry`
+#
+# and `> journal` be `> cd path/to/journal && vmr` (vim most recent)
+
+journal () {
+    cd "/Users/duncanhall/Library/Mobile Documents/com~apple~CloudDocs/Documents/programming/journal"
+
+    d=$(date "+%y%m%d")
+
+    # if journal entry for this date exists:
+    setopt NULL_GLOB
+    for f in ./$d*(.); do
+        echo "$f"
+        v "$f"
+        return
+    done
+    unsetopt NULL_GLOB
+
+    # else:
+    n=$(echo "$*" | tr " " "-")
+    v "$d-$n.md"
+}
